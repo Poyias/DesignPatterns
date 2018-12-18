dc7c802e9afb1e55c6bb40edd392222f975e357b
Andreas <andreaspoyias@gmail.com>
2018-12-18 07:26:04 +0000

    Facade Pattern added

---
+++ Facade.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+class Waiter_Subsystem1
+{
+public:
+  void writeOrder()   { cout << " Waiter writes client's order\n";}
+  void sendToKitchen(){ cout << " Send order to kitchen\n";}
+  void serveCustomer(){ cout << " Yeeei customer is served!!!\n";}
+};
+class Kitchen_Subsystem2
+{
+public:
+    void prepareFood(){ cout << " Cook food\n";}
+    void callWaiter() { cout << " Call Waiter\n";}
+    void washDishes() { cout << " Wash the dishes\n";}
+};
+class Order_Facade
+{
+private:
+    Waiter_Subsystem1 waiter;
+    Kitchen_Subsystem2 kitchen;
+public:
+    void orderFood()
+    {
+        cout << "A series of interdependent calls on various subsystems:\n";
+        waiter.writeOrder();
+        waiter.sendToKitchen();
+        kitchen.prepareFood();
+        kitchen.callWaiter();
+        waiter.serveCustomer();
+        kitchen.washDishes();
+    }
+};
+int main(int argc, char *argv[])
+{
+    // Simple for the client 
+    // no need to know the order or the 
+    // dependencies among various subsystems.
+    Order_Facade facade;
+    facade.orderFood();
+return 0;
+}
+// Output
+// A series of interdependent calls on various subsystems:
+//  Waiter writes client's order
+//  Send order to kitchen
+//  Cook food
+//  Call Waiter
+//  Yeeei customer is served!!!
+//  Wash the dishes
